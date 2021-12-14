943b52d60879f4455b660f470f80a7f45c5e46b9
Niels <nbijl@petersplanet.nl>
2021-12-14 18:52:35 +0100

    initial neural network

--- double-deep-q-learning-networks-in-the-gym/classes/FunctionApproximator.py
+++ double-deep-q-learning-networks-in-the-gym/classes/FunctionApproximator.py
@@ -1,6 +1,15 @@
+from keras import layers, Sequential, optimizers
+
+
 class FunctionApproximator:
     def __init__(self):
-        pass
+        self.model = Sequential()
+        self.model.add(layers.Dense(8, input_shape=(8,)))
+        self.model.add(layers.Dense(32, activation="relu"))
+        self.model.add(layers.Dense(32, activation="relu"))
+        self.model.add(layers.Dense(1, activation="sigmoid"))
+        self.model.compile(optimizer=optimizers.Adam(learning_rate=0.001), loss="rms")
+
 
     def __str__(self):
         return "...."
