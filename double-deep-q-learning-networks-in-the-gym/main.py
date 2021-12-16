663a24d5895533550270c74d1fb66917df6dcc11
Niels <nbijl@petersplanet.nl>
2021-12-16 10:03:17 +0100

    plot reward

--- double-deep-q-learning-networks-in-the-gym/main.py
+++ double-deep-q-learning-networks-in-the-gym/main.py
@@ -1,6 +1,5 @@
-import imp
-import re
 import gym
+import matplotlib.pyplot as plt
 
 from classes.transition import Transition
 from classes.agent import Agent
@@ -11,7 +10,7 @@ env.reset()
 
 tau = 0.8
 
-amount_of_episodes = 10_000
+amount_of_episodes = 20
 max_steps = 1_000
 
 memory_size = 100_000
@@ -20,13 +19,17 @@ policy = EpsilonGreedyPolicy(env.action_space.n, 0.2)
 
 agent = Agent(memory_size, 0, 0, 0)
 
+rewards = []
+
 for i_episode in range(amount_of_episodes):
     state = env.reset()
     print(f'Episode {i_episode}')
+    total_reward = 0
     for t in range(max_steps):
         env.render()
         action = policy.select_action(state, agent.policy_network.model)
         next_state, reward, done, info = env.step(action)
+        total_reward += reward
         transition = Transition(state, action, reward, next_state, done)
         agent.memory.append_memory(transition)
         state = next_state
@@ -37,6 +40,13 @@ for i_episode in range(amount_of_episodes):
     if i_episode % 10 == 0 and i_episode >  0:
         print(f'Copying policy to target {tau}')
         agent.copy_model(tau)
+    rewards.append(total_reward)
 env.close()
 
 agent.policy_network.save()
+
+
+plt.plot([i for i in range(amount_of_episodes)], rewards)
+plt.xlabel('episode')
+plt.ylabel('reward')
+plt.show()
